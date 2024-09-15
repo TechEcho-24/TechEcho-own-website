dfe92537150afdc1c803307706b933a67245a9df
ansh421 <anshsachan421@gmail.com>
2024-09-15 22:45:12 +0530

    feat: :sparkles: commit message

---
+++ tech-web/src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Spline from '@splinetool/react-spline/next';
+
+
+export const Hero = () =>{
+    return (
+        <div>
+
+            <Spline
+            scene="https://prod.spline.design/Z3eH1OnixcGZgcR1/scene.splinecode" 
+          />
+        </div>
+    )
+}
+
+
+
