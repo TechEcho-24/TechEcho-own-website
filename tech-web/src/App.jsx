59e4a54f83d07b3bbab2562318e7a70a610ece16
Jiya Keshwani <jiyakeshwani4@gmail.com>
2024-09-15 23:10:37 +0530

    fix: :bug: fix the spline error

--- tech-web/src/App.jsx
+++ tech-web/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
 import './App.css'
+import { Navbar } from './components/Navbar'
+import { Home } from './pages/Home'
 
 function App() {
   return (
--- tech-web/src/pages/Home.jsx
+++ tech-web/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Hero from '../components/Hero'
+import { Hero } from '../components/Hero'
 
 
-export default Home =()=>{
+export const Home =()=>{
   return(
     <Hero />
   )
