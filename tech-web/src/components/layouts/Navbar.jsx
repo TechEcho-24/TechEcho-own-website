5156a5c85ec84bc502430a10f19cf42e5b1577e3
Anuj Sachan <anujsachan98@gmail.com>
2024-09-19 22:32:54 +0530

    glossy text  color change

--- tech-web/src/components/layouts/Navbar.jsx
+++ tech-web/src/components/layouts/Navbar.jsx
@@ -1,6 +1,6 @@
 import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
 import ToggleSidePanel from "./ToggleSidePanel";
 
 export const Navbar = () => {
@@ -26,67 +26,67 @@ export const Navbar = () => {
     // todo: fix the layout in mobile screens
     <div
       className={`flex justify-between items-center py-6 px-8 ${
-        isScrolled ? "backdrop-grayscale-0" : "bg-transparent"
+        isScrolled ? "bg-transparent" : " backdrop-grayscale-0 bg-[#01135D]"
       }`}
     >
       <div className="flex items-center" onClick={() => setActivePage("home")}>
-        <img src="/assets/logo.png" alt="" className="w-full" />
-        <span className="text-2xl text-white text-shadow font-semibold mt-1 ml-2">
+        <img src="/assets/logo.png" alt="logoIcon" className="w-full " />
+        <span className="md:text-2xl text-xl text-white text-shadow font-semibold mt-1 ml-2">
           TechEcho
         </span>
       </div>
       <ToggleSidePanel />
       <div className="flex">
-      <ul className="xl:flex navbar hidden">
-        <li
-          onClick={() => setActivePage("about")}
-          className={
-            activePage === "about"
-              ? "active cursor-pointer text-white ml-8 text-xl py-2"
-              : "cursor-pointer text-white ml-8 text-xl py-2 mt-1"
-          }
-        >
-          About us
-        </li>
-        <li
-          onClick={() => setActivePage("service")}
-          className={
-            activePage === "service"
-              ? "active cursor-pointer text-white ml-8 text-xl py-2"
-              : "cursor-pointer text-white ml-8 text-xl py-2 mt-1"
-          }
-        >
-          Services
-        </li>
-        <li
-          onClick={() => setActivePage("carrer")}
-          className={
-            activePage === "carrer"
-              ? "active cursor-pointer text-white ml-8 text-xl py-2"
-              : "cursor-pointer text-white ml-8 text-xl py-2 mt-1"
-          }
-        >
-          Career
-        </li>
-        <li
-          onClick={() => setActivePage("contact")}
-          className={
-            activePage === "contact"
-              ? "active cursor-pointer text-white ml-8 text-xl py-2"
-              : "cursor-pointer text-white ml-8 text-xl py-2 mt-1"
-          }
-        >
-          Contact Us
-        </li>
-      </ul>
+        <ul className="xl:flex navbar hidden">
+          <li
+            onClick={() => setActivePage("about")}
+            className={
+              activePage === "about"
+                ? "active cursor-pointer text-white ml-8 text-xl py-2"
+                : "cursor-pointer text-white ml-8 text-xl py-2 mt-1"
+            }
+          >
+            About us
+          </li>
+          <li
+            onClick={() => setActivePage("service")}
+            className={
+              activePage === "service"
+                ? "active cursor-pointer text-white ml-8 text-xl py-2"
+                : "cursor-pointer text-white ml-8 text-xl py-2 mt-1"
+            }
+          >
+            Services
+          </li>
+          <li
+            onClick={() => setActivePage("career")}
+            className={
+              activePage === "career"
+                ? "active cursor-pointer text-white ml-8 text-xl py-2"
+                : "cursor-pointer text-white ml-8 text-xl py-2 mt-1"
+            }
+          >
+            Career
+          </li>
+          <li
+            onClick={() => setActivePage("contact")}
+            className={
+              activePage === "contact"
+                ? "active cursor-pointer text-white ml-8 text-xl py-2"
+                : "cursor-pointer text-white ml-8 text-xl py-2 mt-1"
+            }
+          >
+            Contact Us
+          </li>
+        </ul>
         <button
           className="border-2 border-[#06EFFA] text-white px-6 py-2 ml-8 text-xl cursor-pointer rounded-md"
           onClick={() => setActivePage("login")}
         >
-          <span className="mr-2">Login</span>
+          <span className="mr-2 ">Sign In</span>
           <FontAwesomeIcon icon={faRightToBracket} />
         </button>
-       </div>
+      </div>
     </div>
   );
 };
