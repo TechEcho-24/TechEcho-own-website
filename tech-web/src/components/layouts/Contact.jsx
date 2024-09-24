6fbee1d1b654ce786ccf137c1ed290fd3c385bfd
Anuj Sachan <anujsachan98@gmail.com>
2024-09-25 02:52:58 +0530

    feat: :lipstick: ui fixes

--- tech-web/src/components/layouts/Contact.jsx
+++ tech-web/src/components/layouts/Contact.jsx
@@ -1,55 +1,59 @@
-import React from "react";
+
 
 export const Contact = () => {
   return (
     <>
-      <div className='flex justify-center my-20'>
-        <img src='/assets/contact.png' alt='' />
+      <div className="flex justify-center my-20">
+        <img
+         
+          src="/assets/contact.png"
+          alt="#contact"
+        />
       </div>
-      <form action='#' className='md:w-2/5 mx-auto w-4/5'>
-        <div className='flex md:flex-row flex-col justify-between my-4 items-center'>
+      <form action="#" className="md:w-2/5 mx-auto w-4/5">
+        <div className="flex md:flex-row flex-col justify-between my-4 items-center">
           <input
-            type='text'
-            placeholder='First name'
-            className='w-full p-4 rounded-lg md:mr-4 my-2'
+            type="text"
+            placeholder="First name"
+            className="w-full p-4 rounded-lg md:mr-4 my-2"
           />
           <input
-            type='text'
-            placeholder='Last name'
-            className='w-full p-4 rounded-lg md:ml-4 my-2'
-          />
-        </div>
-        <div className='flex md:flex-row flex-col justify-between my-4 items-center'>
-          <input
-            type='text'
-            placeholder='Email'
-            className='w-full p-4 rounded-lg md:mr-4 my-2'
-          />
-          <input
-            type='text'
-            placeholder='Mobile no.'
-            className='w-full p-4 rounded-lg md:ml-4 my-2'
+            type="text"
+            placeholder="Last name"
+            className="w-full p-4 rounded-lg md:ml-4 my-2"
           />
         </div>
-        <div className='my-4'>
+        <div className="flex md:flex-row flex-col justify-between md:my-4 my-2 items-center">
           <input
-            type='text'
-            placeholder='Email Subject'
-            className='w-full rounded-lg p-4'
+            type="text"
+            placeholder="Email"
+            className="w-full p-4 rounded-lg md:mr-4 my-2"
+          />
+          <input
+            type="text"
+            placeholder="Mobile no."
+            className="w-full p-4 rounded-lg md:ml-4 my-2"
           />
         </div>
-        <div className='my-4'>
+        <div className="my-4">
+          <input
+            type="text"
+            placeholder="Email Subject"
+            className="w-full rounded-lg p-4"
+          />
+        </div>
+        <div className="my-4">
           <textarea
-            name='textarea'
-            className='w-full rounded-lg text-2xl p-4'
+            name="textarea"
+            className="w-full rounded-lg text-2xl p-4"
             rows={5}
             cols={50}
-            placeholder='Your Message'
+            placeholder="Your Message"
           ></textarea>
         </div>
         <input
-          type='submit'
-          className='border-4 border-[#488edf] w-full text-white p-4 rounded-lg text-2xl hover:bg-[#488edf]'
+          type="submit"
+          className="border-4 border-[#488edf] w-full text-white p-4 rounded-lg text-2xl hover:bg-[#488edf]"
         />
       </form>
     </>
--- tech-web/src/components/layouts/Intro.jsx
+++ tech-web/src/components/layouts/Intro.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-
 export const Intro = () => {
   return (
     <>
-      <div className='flex md:flex-row flex-col py-32 items-center justify-between md:px-32 px-10'>
-        <div className='text-white basis-1/2'>
-          <h2 className='text-5xl font-extrabold my-6'>What We Offer</h2>
-          <p className='text-2xl leading-10'>
+      <div className="flex md:flex-row flex-col md:py-32 py-6 items-center justify-between md:px-32 px-10">
+        <div className="text-white basis-1/2">
+          <h2 className="md:text-4xl text-2xl font-extrabold my-6">
+            What We Offer
+          </h2>
+          <p className="md:text-xl lg:text-xl  text-md md:leading-8">
             At TechEcho, we are at the forefront of innovation, crafting
             cutting-edge technological solutions tailored to meet the needs of a
             dynamic and ever-evolving market. Based in Kanpur, Uttar Pradesh,
@@ -15,12 +15,12 @@ export const Intro = () => {
             landscape. From software development to AI-driven solutions, we are
             dedicated to driving progress through tech.
           </p>
-          <button className='md:py-6 md:px-10 p-4 text-white border-4 border-[#488edf] my-14 md:text-4xl font-bold rounded-lg hover:bg-[#488edf]'>
+          <button className="md:py-3 md:px-6 p-4 text-white border-4 border-[#488edf] my-14 md:text-xl font-semibold rounded-lg hover:bg-[#488edf]">
             Connect With Us
           </button>
         </div>
-        <div className='basis-5/12'>
-          <img src='/assets/intro.svg' alt='' />
+        <div className="basis-5/12">
+          <img src="/assets/intro.svg" alt="" />
         </div>
       </div>
     </>
