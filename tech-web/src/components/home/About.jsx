09bf36c530fb16ace3650f83b35c4c3db524b712
Jiya Keshwani <jiyakeshwani4@gmail.com>
2024-10-16 00:18:59 +0530

    feat: :fire: ui fixes

--- tech-web/src/components/home/About.jsx
+++ tech-web/src/components/home/About.jsx
@@ -7,13 +7,13 @@ export const About = () => {
       {/* team  */}
       <div className='flex md:flex-row flex-col md:mx-32 mx-10 my-4 md:my-10'>
         <div className='md:basis-1/2 flex md:relative'>
-          <div className='bg-[#488edf] w-40 h-48 hidden md:block md:w-[8rem] md:h-46'></div>
-          <figure className='md:relative -left-20 top-11 md:w-3/4'>
-            <img src='/assets/team.png' alt='team' className='w-full' />
+          <div className='bg-[#488edf] md:w-32 md:h-48  lg:h-72 hidden md:block'></div>
+          <figure className='md:relative -left-16 top-11 md:w-3/4'>
+            <img src='/assets/team.png' alt='team' className='w-full md:h-[50%] lg:h-full ' />
           </figure>
         </div>
         <div className='md:basis-1/2 text-white'>
-          <p className='my-4 text-sm md:text-xl'>
+          <p className='my-4 text-sm md:text-sm lg-text-xl'>
             Located in the thriving tech hub of Kanpur, TechEcho boasts a team
             of experts who bring diverse experiences from various technology
             sectors. Our team's collective expertise in fields like machine
@@ -31,16 +31,16 @@ export const About = () => {
       {/* carrier  */}
       <div className='flex md:flex-row-reverse flex-col md:mx-32 mx-10 mt-20 items-center'>
         <div className='md:basis-1/2 flex'>
-          <figure className='md:relative -right-24 top-8 md:w-3/4'>
-            <img src='/assets/carrier.png' alt='team' className='w-full' />
+          <figure className='md:relative -right-20 top-11 md:w-3/4'>
+            <img src='/assets/carrier.png' alt='team' className='md:w-full md:h-full object-cover' />
           </figure>
-          <div className='bg-[#488edf] w-40 h-48 hidden md:block md:w-[8rem] md:h-46'></div>
+          <div className='bg-[#488edf] md:w-32 md:h-48  lg:h-72 hidden md:block'></div>
         </div>
         <div className='md:basis-1/2 text-white'>
           <div className='flex my-10'>
             <img src='/assets/carrierHead.png' alt='' />
           </div>
-          <p className='my-4 text-sm md:text-xl'>
+          <p className='my-4 text-sm md:text-sm lg-text-xl'>
             At TechEcho, our Career Section offers top-tier Full Stack
             Development and UX/UI Design courses, guided by industry experts in
             a collaborative and innovative environment. We ensure hands-on
@@ -56,16 +56,16 @@ export const About = () => {
       {/* founder  */}
       <div className='flex md:flex-row flex-col md:mx-32 mx-10 py-5 items-center'>
         <div className='md:basis-1/2 flex md:relative'>
-          <div className='bg-[#488edf] md:w-[8rem] md:h-46 w-40 h-48 hidden md:block'></div>
-          <figure className='md:relative -left-20 top-11 md:w-3/4'>
-            <img src='/assets/founder.png' alt='team' className='w-full' />
+          <div className='bg-[#488edf] md:w-32 md:h-48  lg:h-72 hidden md:block'></div>
+          <figure className='md:relative -left-16 top-11 md:w-3/4'>
+            <img src='/assets/founder.png' alt='team' className='md:w-full md:h-full  object-fill' />
           </figure>
         </div>
         <div className='md:basis-1/2 text-white'>
           <div className='flex my-10'>
             <img src='/assets/founderHead.png' alt='' />
           </div>
-          <p className='my-4 text-sm md:text-xl'>
+          <p className='my-4 text-sm md:text-sm lg-text-xl'>
             At TechEcho, we are at the forefront of innovation, crafting
             cutting-edge technological solutions tailored to meet the needs of a
             dynamic and ever-evolving market. Based in Kanpur, Uttar Pradesh,
