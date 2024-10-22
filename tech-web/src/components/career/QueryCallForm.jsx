21146070ae2efc5ae9c8425639cfc3e76ee81743
Jiya Keshwani <jiyakeshwani4@gmail.com>
2024-10-23 00:41:57 +0530

    fix: :art:

--- tech-web/src/components/career/QueryCallForm.jsx
+++ tech-web/src/components/career/QueryCallForm.jsx
@@ -1,5 +1,5 @@
-import { faUserGraduate } from "@fortawesome/free-solid-svg-icons";
-import React, { useState, useEffect } from "react";
+
+import { useEffect, useState } from "react";
 
 export const QueryCallForm = () => {
   const [userData, setUserData] = useState({
